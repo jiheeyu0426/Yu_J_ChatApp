6cb4924f9d61c6e191d5ed1fff763f68b74b8960
jiheeyu0426 <jiheeyu0426@github.com>
2021-02-18 11:12:03 +0900

    sendingmessage

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const messenger = require('socket.io')();
 
 const app = express();
 
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 const port = process.env.PORT || 5050;
 
@@ -25,10 +25,19 @@ const server = app.listen(port, () => {
 messenger.attach(server);
 
 // socket is the individual connection - the caller
-messenger.on("connection", (socket) => {
+messenger.on('connection', (socket) => {
 	console.log(`a user connected: ${socket.id}`);
 
+	// send the connected user their assigned ID
+	socket.emit("connected", { sID: `${socket.id}`, message: 'new connection'});
+
+	socket.on('chatmessage', function(msg) {
+		console.log(msg);
+
+		messenger.emit('message', { id: socket.id, message: msg });
+	});
+	
 	socket.on('disconnect', () => {
-		console.log('a user has disconnected')
+		console.log('a user has disconnected');
 	})
-})
+});
